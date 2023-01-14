d90c15fcd561413391bc7da4b321e6b4e856cf61
Uwe Kremer <uwekremer1971@gmx.de>
2023-01-14 16:47:23 +0100

    aufteilung und benennung

--- src/App.js
+++ src/App.js
@@ -3,7 +3,28 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className='partitionScreen'>
+        <div className="player1">
+          <div>Player 1</div>
+          <div>Start Score: </div>
+          <div>Aktueller Score: </div>
+        </div>
+        <div className="player2">
+          <div>Player 2</div>
+          <div>Start Score: </div>
+          <div>Aktueller Score: </div>
+        </div>
+        <div className="player3">
+          <div>Player 3</div>
+          <div>Start Score: </div>
+          <div>Aktueller Score: </div>
+        </div>
+        <div className="player4">
+          <div>Player 4</div>
+          <div>Start Score: </div>
+          <div>Aktueller Score: </div>
+        </div>
+      </div>
     </div>
   );
 }
